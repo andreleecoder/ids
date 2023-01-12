4ad829281b2241299733c0134d4d1c73e0edd37f
andre Lee <43418927+andreleecoder@users.noreply.github.com>
2023-01-12 10:46:27 -0300

    Add files via upload

---
+++ src/components/PaletaCores.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-card flat>
+    <q-card flat class="row justify-between">
+      <q-card-section class="col text-center text-subtitle2">
+        Cores
+      </q-card-section>
+      <q-card-section class="col text-center text-subtitle2 text-bold">
+        variavel
+      </q-card-section>
+      <q-card-section class="col text-center text-subtitle2 text-bold">
+        HEX
+      </q-card-section>
+      <q-card-section class="col text-center text-subtitle2 text-bold">
+        RGB
+      </q-card-section>
+    </q-card>
+    <q-separator color="primary"></q-separator>
+    <q-card flat class="row justify-between">
+      <q-card-section class="col text-center bg-primary" cor>
+        {{ cor }}</q-card-section
+      >
+      <q-card-section
+        class="col text-center text-subtitle2 text-weight-bolder text-primary"
+        variavel
+      >
+        {{ variavel }}
+      </q-card-section>
+      <q-card-section
+        class="col text-center text-subtitle2 text-weight-bolder text-primary"
+        hex
+      >
+        {{ hex }}
+      </q-card-section>
+      <q-card-section
+        class="col text-center text-subtitle2 text-weight-bolder text-primary"
+        rgb
+      >
+        {{ rgb }}
+      </q-card-section>
+    </q-card>
+    <q-separator color="primary"></q-separator>
+  </q-card>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PaletaCores',
+  props: {
+    cor: {
+      type: hex,
+      default: '',
+    },
+
+    variavel: {
+      type: String,
+      default: '',
+    },
+
+    hex: {
+      type: hex,
+      default: '',
+    },
+
+    rgb: {
+      type: String,
+      default: '',
+    },
+  },
+});
+</script>
