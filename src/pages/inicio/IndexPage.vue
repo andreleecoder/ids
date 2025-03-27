1118bb92f7b340453f69d98839a651f5315e5588
andre Lee <43418927+andreleecoder@users.noreply.github.com>
2025-03-27 15:04:12 -0300

    Add files via upload

--- src/pages/inicio/IndexPage.vue
+++ src/pages/inicio/IndexPage.vue
@@ -1,56 +1,217 @@
 <script setup>
 import { ref } from 'vue'
-import CardVantagens from 'src/components/inicio/CardVantagens.vue';
-const listaCardVantagens = ref(
-  [
-    {
-      titulo: "Consistência",
-      conteudo: "Ele facilita decisões de design, colabora para o alinhamento dos times de produto e acelera o desenvolvimento,permitindo que as pessoas foquem seus esforços em soluções mais estratégicas.",
-    },
-    {
-      titulo: "Velocidade",
-      conteudo: " Quando designers e desenvolvedores melhoram sua forma de se comunicar e colaborar, o resultado é menos tempo executando tarefas repetitivas e tomando decisões e mais velocidade nas entregas.",
-    },
-    {
-      titulo: "Qualidade",
-      conteudo: " Nossos padrões são desenvolvidos levando em conta as necessidades do produto, dos usuários e seguindo boas práticas de front-end. Além disso são constantemente testados e validados, gerando melhoria contínua e maior qualidade.",
-    },
-    {
-      titulo: "Evolução",
-      conteudo: " Produtos digitais precisam evoluir constantemente e tornar esse processo mais simples é um dos objetivos do Design System. Com um sistema centralizado, atualizar e melhorar interfaces torna-se mais fácil e ágil. Retrabalho não precisa ser o caminho para a evolução.",
-    },
-    {
-      titulo: "Melhor Experiência",
-      conteudo: "Ter padrões claros, consistentes e efetivos traz benefícios também para os nossos usuários, que vão ter mais facilidade no aprendizado e utilização dos nossos produtos.",
-    },
+import CardsNavegacao from 'src/components/inicio/CardsNavegacao.vue';
 
-  ])
-let cards = listaCardVantagens
+
+let listaRotaDev = ref([
+  {
+    title: 'BPMN ',
+    caption: 'Guia de Boas práticas para a criação de fluxos usando BPMN ',
+    icon: 'account_tree',
+    link: 'bpmn',
+    isActive: 'bpmn',
+  },
+  {
+    title: 'Boas Práticas',
+    caption: 'Guia de Boas práticas para o desenvolvimento de interfaces ',
+    icon: 'thumb_up_alt',
+    link: 'boasPraticas',
+    isActive: 'boasPraticas',
+  },
+  {
+    title: 'Getting Started',
+    caption: 'Tecnologias de frontend usadas na illimitar ',
+    icon: 'play_circle',
+    link: 'start',
+
+  },
+  {
+    title: 'Ícones',
+    caption: 'Bibliotecas de icones usadas na illimitar para desenvolvimento',
+    icon: 'add_reaction',
+    link: 'icones',
+    isActive: 'icones',
+  },
+  {
+    title: 'variáveis de cor',
+    caption: 'Definindo váriáveis de cor nos frameworks',
+    icon: 'colorize',
+    link: 'varCor',
+
+  },
+
+
+])
+
+let listaRotaDevComponentes = ref([
+  {
+    title: 'Botões',
+    caption: 'Como usar botões da melhor maneira',
+    icon: 'call_to_action',
+    link: 'botoes',
+    isActive: 'botoes',
+  },
+  {
+    title: 'Barras',
+    caption: 'Como usar as barras da melhor maneira',
+    icon: 'view_day',
+    link: 'barras',
+    isActive: 'barras',
+  },
+  {
+    title: 'Tabs',
+    caption: 'Como usar as Tabs da melhor maneira',
+    icon: 'padding',
+    link: 'tab',
+    isActive: 'tab',
+  },
+
+  // {
+  //   title: 'Modelos de layout',
+  //   caption: 'Sugestões de arquitetura de layout',
+  //   icon: 'space_dashboard',
+  //   link: 'modeloLayout',
+  //   isActive: 'modeloLayout',
+  // },
+])
+
+let listaRotaEscrita = ref([
+  {
+    title: 'Princípios',
+    caption: 'Princípios de conteúdo escrito',
+    icon: 'menu_book',
+    link: 'principios',
+    isActive: 'principios',
+  },
+  {
+    title: 'Preferências e Configurações',
+    caption: 'Textos de configurações',
+    icon: 'settings',
+    link: 'configuracoes',
+    isActive: 'configuracoes',
+  },
+  {
+    title: 'Mensagens de Erro',
+    caption: 'Criando mensagens de erro',
+    icon: 'warning',
+    link: 'mensagemErro',
+    isActive: 'mensagemErro',
+  },
+  {
+    title: 'Mensagens de Sucesso',
+    caption: 'Criando mensagens de Sucesso',
+    icon: 'check_circle',
+    link: 'mensagemSucesso',
+    isActive: 'mensagemSucesso',
+
+  },
+])
+let listaRotaEstilo = ref([
+  {
+    title: 'Cores',
+    caption: 'Paleta de cores',
+    icon: 'palette',
+    link: 'cores',
+    isActive: 'cores',
+  },
+  {
+    title: 'Marca',
+    caption: 'Manual Marca',
+    icon: 'diamond',
+    link: 'marca',
+    isActive: 'marca',
+  },
+])
+//itens do drawer de navegação
+let rotaDev = listaRotaDev
+let rotaDevComponentes = listaRotaDevComponentes
+let rotaEscrita = listaRotaEscrita
+let rotaEstilo = listaRotaEstilo
+
+
+
+//  estado dos Componentes expansion items
+let expandirItens = ref({
+  dev: true,
+  devComponentes: true,
+  escrita: true,
+  estilo: true,
+});
+
+
+// Variável que está no ternário do ícone recolher tudo
+
+let aberto = ref(false);
+
+// Verifica se todas as propriedades do objeto expandirItens têm o mesmo valor
+function todasAsPropriedadesIguais(obj) {
+  const valores = Object.values(obj);
+  return valores.every((val, i, arr) => val === arr[0]);
+}
+
+// Fecha tudo
+let expandir = expandirItens;
+let mudarEstado = () => {
+  if (!todasAsPropriedadesIguais(expandirItens.value)) {
+    // Se as propriedades não estiverem iguais, defina todas como false
+    for (let propriedade in expandirItens.value) {
+      expandirItens.value[propriedade] = false;
+    }
+  } else {
+    // Caso contrário, alterne o estado de todas as propriedades
+    for (let propriedade in expandirItens.value) {
+      expandirItens.value[propriedade] = !expandirItens.value[propriedade];
+    }
+  }
+  // Muda valor da variável para o oposto do estado atual
+  aberto.value = !aberto.value;
+};
+
+let mudar = mudarEstado;
 
 </script>
 <template>
-  <q-page class="fundo-paginas column q-px-lg q-py-lg flex-center">
-    <img style="width:40%;" alt="Quasar logo"
-      src="https://cloud.illimitar.com.br/apps/files_sharing/publicpreview/img-ids?file=/logo-illimitar-vertical-2023.png&fileId=963370&x=1920&y=1080&a=true" />
-    <h4 class="i-h4 q-mb-none">illimitar Design system</h4>
-    <div class="column q-px-lg q-my-lg ">
+  <q-page class=" column q-px-sm q-py-md ">
+    <img style="width:40%;" class="col-md-5 col-xs self-center" alt="Quasar logo" src="../../assets/inicio/logo-illimitar-vertical-2023.png" />
+    <h4 class="i-h4 text-center q-mb-none">illimitar Design system</h4>
+    <div class="column q-px-md q-my-lg ">
 
-      <p class="i-subtitle1">
-        O Design System fornece recursos e
-        guias para construir interfaces consistentes,
-        acessíveis e com foco em oferecer uma ótima experiência aos nossos usuários.
+      <p class=" text-center i-subtitle1">
+        Um design system é um conjunto de padrões, diretrizes, componentes e princípios de design que ajudam a manter a
+        consistência e a coesão visual em um produto digital. Ele serve como uma fonte única de verdade para designers,
+        desenvolvedores e outros profissionais envolvidos no processo de criação de produtos.
       </p>
-      <p class="i-subtitle1">
-        Ele facilita decisões de design, colabora para o
-        alinhamento dos times de produto e acelera o desenvolvimento,
-        permitindo que as pessoas foquem seus esforços em soluções mais estratégicas.
-      </p>
-      <h5 class=" i-h5 q-mb-none">Vantagens ao adotar um design system</h5>
-
     </div>
-    <!--cards vantagens-->
-    <CardVantagens v-for="lista in cards" :key="lista.titulo" v-bind="lista" />
+    <div class="col flex justify-end">
+
+      <q-btn :label="aberto ? 'expandir' : 'recolher'" :icon="aberto ? 'expand_more' : 'expand_less'" color="light" flat @click="mudar" />
+    </div>
+
+    <q-expansion-item v-model="expandir.dev" :expanded="expandir.dev" header-class="i-h6" class="col " icon="code" label="Desenvolvimento">
+      <div class="row q-gutter-sm">
+        <CardsNavegacao v-for="link in rotaDev" :key="link.title" v-bind="link" />
+      </div>
+      <!--expansor componentes-->
+      <q-expansion-item v-model="expandir.devComponentes" :expanded="expandir.devComponentes" class="col " header-class="i-h6" icon="smart_button" label="Componentes">
+
+        <div class="row col q-gutter-sm">
+          <CardsNavegacao v-for="link in rotaDevComponentes" :key="link.title" v-bind="link" />
+        </div>
+      </q-expansion-item>
+    </q-expansion-item>
+    <q-separator />
+    <q-expansion-item v-model="expandir.escrita" :expanded="expandir.escrita" header-class="i-h6" class="col " icon="edit_note" label="Escrita">
+      <div class="row q-gutter-sm">
+        <CardsNavegacao :isActive="link.isActive" v-for="link in rotaEscrita" :key="link.title" v-bind="link" />
+      </div>
+    </q-expansion-item>
+    <q-separator />
+    <!--estilo-->
+    <q-expansion-item v-model="expandir.estilo" :expanded="expandir.estilo" header-class="i-h6" class="col " icon="style" label="Estilo">
+      <div class="row q-gutter-sm">
+        <CardsNavegacao :isActive="link.isActive" v-for="link in rotaEstilo" :key="link.title" v-bind="link" />
+      </div>
+    </q-expansion-item>
   </q-page>
 </template>
-
 
